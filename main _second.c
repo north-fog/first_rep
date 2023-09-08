cdc6424068a4566f5c943f1bec3c0bc0bbcd17f8
north-fog <severfog5690jp@gmail.com>
2023-09-08 16:13:19 +0300

    help

---
+++ main _second.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void main()
+{   
+    int a,b;
+    int c;
+    printf("Ввеите первое число:\n");
+    scanf("%d",&a);
+    printf("Ввеите второе число:\n");
+    scanf("%d",&b);
+    c=a+b;
+    printf("Сумма чисел = %d",c);
+}
